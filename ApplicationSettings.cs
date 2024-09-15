8333614673534ceef72a651d2875d4494aa478cb
Armando Sanchez <sanchez.armando29@gmail.com>
2024-09-15 16:29:41 +0200

    Init Commit

---
+++ ApplicationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Options;
+
+public class ApplicationSettings
+{
+    public string AppName { get; set; }
+    public string Version { get; set; }
+}
---
+++ Controllers/AppSettingsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Options.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppSettingsController : ControllerBase
+{
+    private readonly ApplicationSettings _iOptions;
+    private readonly IOptionsMonitor<ApplicationSettings> _iOptionsMonitor;
+    private readonly IOptionsSnapshot<ApplicationSettings> _iOptionsSnapshot;
+
+    public AppSettingsController(IOptions<ApplicationSettings> options, IOptionsMonitor<ApplicationSettings> iOptionsMonitor, IOptionsSnapshot<ApplicationSettings> optionsSnapshot)
+    {
+        _iOptions = options.Value;
+        _iOptionsMonitor = iOptionsMonitor;
+        _iOptionsSnapshot = optionsSnapshot;
+    }
+
+    [HttpGet("GetOptions")]
+    public IActionResult GetOptions()
+    {
+        return Ok(_iOptions);
+    }
+
+    [HttpGet("GetOptionsSnapshot")]
+    public IActionResult GetOptionsSnapshot()
+    {
+        return Ok(_iOptionsSnapshot.Value);
+    }
+
+    [HttpGet("GetOptionsMonitor")]
+    public IActionResult GetOptionsMonitor()
+    {
+        return Ok(_iOptionsMonitor.CurrentValue);
+    }
+}
